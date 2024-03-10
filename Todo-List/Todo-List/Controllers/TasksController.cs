faf1eee887ff6b655aef87d62b1192e25e93678a
GerodiazRogel-BSIT32E1 <gerodiazrogel1@gmail.com>
2024-03-11 00:43:25 +0800

    Create, Delete Task 3/11/2024

--- Todo-List/Todo-List/Controllers/TasksController.cs
+++ Todo-List/Todo-List/Controllers/TasksController.cs
@@ -1,8 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using TodoListApp.Models;
-
+using Todo_List.Models;
+using Todo_List.Data;
 
 namespace Todo_List.Controllers
 {
@@ -28,20 +28,104 @@ namespace Todo_List.Controllers
         }
 
         // POST: Tasks/Create
-        // POST: Tasks/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Description,DueDate,PriorityLevel,IsComplete,Category")] TaskModel task)
         {
             if (ModelState.IsValid)
             {
-                _context.Tasks.Add(task);
+                _context.Add(task);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index", "Home");
             }
             return View(task);
         }
 
-        // Other action methods...
+        // GET: Tasks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return View(task);
+        }
+
+        // POST: Tasks/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Description,DueDate,PriorityLevel,IsComplete,Category")] TaskModel task)
+        {
+            if (id != task.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(task);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskExists(task.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View(task);
+        }
+
+        // GET: Tasks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        // POST: Tasks/DeleteConfirmed/5
+        [HttpPost, ActionName("DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id) // Add the id parameter here
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
     }
 }
