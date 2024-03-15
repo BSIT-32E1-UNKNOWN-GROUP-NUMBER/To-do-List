8fe80819c3e9b43e66e859758ebf18136307f486
GerodiazRogel-BSIT32E1 <160821545+GerodiazRogel-BSIT32E1@users.noreply.github.com>
2024-03-15 14:16:51 +0800

    Update getTask.js

--- Todo-List/Todo-List/wwwroot/js/getTask.js
+++ Todo-List/Todo-List/wwwroot/js/getTask.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
     $('.edit-icon').click(function () {
-        var taskId = parseInt($(this).data('id')); // Parse the id to an integer
+        var taskId = parseInt($(this).data('id'));
 
         $.ajax({
-            url: '/Tasks/GetTask/' + taskId, // URL of the method that returns task data
+            url: '/Tasks/GetTask/' + taskId, 
             type: 'GET',
             success: function (task) {
                 $('.input-field[name="Description"]').val(task.description);
                 $('.select-input[name="Category"]').val(task.category);
                 $('#dateTimeInput').val(task.dueDate);
-                // Repeat for all other input fields
+                
             },
             error: function (jqXHR, textStatus, errorThrown) {
-                console.error(textStatus, errorThrown); // Log any errors to the console
+                console.error(textStatus, errorThrown);
             }
         });
     });
